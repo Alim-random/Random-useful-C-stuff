0467cf9578cc3901b0927cacc78fd2300c75017f
Alim-random <89221797+Alim-random@users.noreply.github.com>
2021-08-20 23:58:04 -0400

    Create USEFUL STUFF.cpp

---
+++ USEFUL STUFF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//To find things easily, I have a table of contents below:
+
+//For mac users, use (cmd + F) and type in the title of the table of contents in which you want to find. 
+//For windows or other users, use (ctrl + F) and do the same thing
+
+/*Table of Contents:
+*
+*/
+
+
+
+
+
+
+}
