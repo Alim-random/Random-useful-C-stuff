eded528a4655ef148607372b8b38e7b8a59fe6cf
Alim-random <89221797+Alim-random@users.noreply.github.com>
2021-08-21 00:08:29 -0400

    Update USEFUL STUFF.cpp

--- USEFUL STUFF.cpp
+++ USEFUL STUFF.cpp
@@ -6,13 +6,33 @@ int main(){
 //To find things easily, I have a table of contents below:
 
 //For mac users, use (cmd + F) and type in the title of the table of contents in which you want to find. 
-//For windows or other users, use (ctrl + F) and do the same thing
+//For windows or other users, use (ctrl + F) and do the same thing. Then scroll down or press the arrow
+//on new tab to get to next.
 
+//copy what you need and use (cmd or ctrl + /) to uncomment it. Enjoy :)
+	
 /*Table of Contents:
-*
+* 
+* Maximum101
+* Minimum101
+* Sorting101
+* 
 */
 
+	
 
+//Maximum101:
+	//long double max = -1e18; //low dummy value
+	//int checkIfMax;
+	//long long until;
+	//cin >> checkIfMax;
+	//cin >> until;
+	//for(int i = 0; i<until; i++){
+	//	if(checkIfMax>max){
+	//		max = checkIfMax;
+	//	}
+	//}
+	//cout << max;
 
 
 
